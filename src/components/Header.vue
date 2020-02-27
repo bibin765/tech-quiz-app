105a2207d83b008bff648fea117ead350583f8e8
BIBIN OOMMEN MATHEW <bibin765@gmail.com>
2020-02-27 14:29:02 +0530

    added questions

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>TechQuizApp</b></b-nav-item>
+            <b-nav-item disabled>Counter 2/50</b-nav-item>
+        </b-nav>
+    </div>
+</template>
