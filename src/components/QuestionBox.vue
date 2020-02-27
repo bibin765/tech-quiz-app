e02b14288d82878e1144932e830f24eb58d2b9ee
BIBIN OOMMEN MATHEW <bibin765@gmail.com>
2020-02-27 16:07:12 +0530

    added answers

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,9 +7,16 @@
 
             <hr class="my-4">
 
-            <p>
-            Options
-            </p>
+            <b-list-group>
+                <b-list-group-item
+                    v-for="(answer,index) in answers" 
+                    :key="index" 
+                    @click.prevent="selectAnswer(index)"
+                    :class="[selectedIndex === index ? 'selected' : '']"
+                >
+                    {{answer}}
+                </b-list-group-item>
+            </b-list-group> 
 
             <b-button variant="primary" href="#">Submit</b-button>
             <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -18,10 +25,47 @@
 </template>
 
 <script>
+
+import _ from 'lodash';
+
 export default {
     props : {
         currentQuestion : Object,
         next:Function,
+    },
+    data(){
+        return(
+            {
+                selectedIndex : null,
+                shuffledAnswers : [],
+            }
+        )
+    },
+    methods:{
+        selectAnswer(index){
+            this.selectedIndex = index
+            console.log(index)
+        },
+        shuffleAnswers(){
+            let answers = [...this.currentQuestion.incorrect_answers , this.currentQuestion.correct_answer];
+            this.shuffledAnswers = _.shuffle(answers);
+        },
+    },
+    computed:{
+        answers(){
+        let answers = [...this.currentQuestion.incorrect_answers]
+        answers.push(this.currentQuestion.correct_answer)
+        return answers
+        }
+    },
+    watch:{
+        currentQuestion: {
+            immediate : true,
+            handler(){
+                this.selectedIndex = null
+                this.shuffleAnswers();
+            }
+        }
     },
     mounted(){
         console.log(this.currentQuestion);
@@ -29,3 +73,26 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    .list-group {
+        margin-bottom: 15px;
+    }
+    .btn{
+        margin: 0 5px;
+    }
+    .list-group-item:hover{
+        background-color: #eee;
+        cursor: pointer;
+    }
+    .selected,.selected:hover{
+        background-color: lightblue;
+    }
+    .correct{
+        background-color: lightgreen
+    }
+    .incorrect{
+        background-color: red;
+    }
+
+</style>
